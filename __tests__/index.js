e6cd6358395369ebe09dd7cc1329557f0e6e72d8
Nils Schönwald <nils@schoenwald.media>
2016-11-02 17:43:19 +0100

    Fix selector-bem-pattern

--- __tests__/index.js
+++ __tests__/index.js
@@ -3,7 +3,9 @@ import stylelint from "stylelint"
 import test from "ava"
 
 const validCss = (
-`/**
+`/** @define Selector */
+
+/**
  * Multi-line comment
  */
 
@@ -83,7 +85,7 @@ test("no warnings with valid css", t => {
   .then(data => {
     const { errored, results } = data
     const { warnings } = results[0]
-    t.notOk(errored, "no errored")
+    t.falsy(errored, "no errored")
     t.is(warnings.length, 0, "flags no warnings")
   })
 })
@@ -96,7 +98,7 @@ test("a warning with invalid css", t => {
   .then(data => {
     const { errored, results } = data
     const { warnings } = results[0]
-    t.ok(errored, "errored")
+    t.truthy(errored, "errored")
     t.is(warnings.length, 1, "flags one warning")
     t.is(warnings[0].text, "Unexpected leading zero (number-leading-zero)", "correct warning text")
   })
